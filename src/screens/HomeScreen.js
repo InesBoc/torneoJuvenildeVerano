8fa60325440218f6b8ea11b626a2921769ccefe8
InesBoc <mariainesbocanera@yahoo.com.ar>
2026-02-07 15:07:29 -0300

    padding botton

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -16,67 +16,68 @@ export default function HomeScreen({ navigation }) {
   const scrollHeight = height - 100;
 
   return (
- <View style={{ flex: 1, backgroundColor: '#fff' }}>
-   <View style={{ flex: 1 }}>
-    <ScrollView 
-        contentContainerStyle={[globalStyles.scrollContent, { paddingBottom: 20 }]}
+  <View style={{ flex: 1, backgroundColor: '#fff' }}>
+
+    <View style={{ flex: 1 }}>
+      <ScrollView 
+        contentContainerStyle={{ paddingBottom: 20 }}
         showsVerticalScrollIndicator={true}
       >
         <View style={globalStyles.scrollContent}>
-        <View style={{ alignItems: 'center', marginBottom: 20 }}>
-          <Text style={globalStyles.title}>Torneo Juvenil de Verano 2026</Text>
-          <Text style={[globalStyles.title, { fontSize: 20, marginVertical: 10 }]}>14 y 15 de Febrero</Text>
-          <Text style={globalStyles.subtitle}>
-            ¡Bienvenidos al 1° Torneo Juvenil de Verano de Tigres Rugby Club!
-            Este Torneo fue creado para hacer un cierre de Pretemporada reforzando tanto el aspecto fisico-deportivo como la integración del grupo y el trabajo en equipo.
-          </Text>
-        </View>
+          <View style={{ alignItems: 'center', marginBottom: 20 }}>
+            <Text style={globalStyles.title}>Torneo Juvenil de Verano 2026</Text>
+            <Text style={[globalStyles.title, { fontSize: 20, marginVertical: 10 }]}>14 y 15 de Febrero</Text>
+            <Text style={globalStyles.subtitle}>
+              ¡Bienvenidos al 1° Torneo Juvenil de Verano de Tigres Rugby Club!
+              Este Torneo fue creado para hacer un cierre de Pretemporada...
+            </Text>
+          </View>
 
-        <View style={{ marginBottom: 25 }}>
-          <Text style={[globalStyles.title, { fontSize: 18, textAlign: 'center', marginVertical: 10 }]}>
-            Ubicación del Club
-          </Text>
-          <MapaProxy styles={styles} />
-        </View>
+          <View style={{ marginBottom: 25 }}>
+            <Text style={[globalStyles.title, { fontSize: 18, textAlign: 'center', marginVertical: 10 }]}>
+              Ubicación del Club
+            </Text>
+            <MapaProxy styles={styles} />
+          </View>
 
-        <View style={{ marginBottom: 20 }}>
-          <TouchableOpacity 
-            style={[globalStyles.input, { backgroundColor: '#f34343', alignItems: 'center' }]} 
-            onPress={() => navigation.navigate('DatosClub')}
-          >
-            <Text style={globalStyles.btnText}>Nueva Inscripción</Text>
-          </TouchableOpacity>
+          <View style={{ marginBottom: 20 }}>
+            <TouchableOpacity 
+              style={[globalStyles.input, { backgroundColor: '#f34343', alignItems: 'center' }]} 
+              onPress={() => navigation.navigate('DatosClub')}
+            >
+              <Text style={globalStyles.btnText}>Nueva Inscripción</Text>
+            </TouchableOpacity>
 
-          <TouchableOpacity 
-            style={[globalStyles.input, { backgroundColor: '#f98975', alignItems: 'center' }]} 
-            onPress={() => navigation.navigate('MisInscripciones')}
-          >
-            <Text style={globalStyles.btnText}>Ver Mis Inscripciones</Text>
-          </TouchableOpacity>
+            <TouchableOpacity 
+              style={[globalStyles.input, { backgroundColor: '#f98975', alignItems: 'center' }]} 
+              onPress={() => navigation.navigate('MisInscripciones')}
+            >
+              <Text style={globalStyles.btnText}>Ver Mis Inscripciones</Text>
+            </TouchableOpacity>
 
-          <TouchableOpacity 
-            style={[globalStyles.input, { backgroundColor: '#f7a684', alignItems: 'center' }]}
-            onPress={() => navigation.navigate('Fixture')}
-          >
-            <Text style={globalStyles.btnText}>🏆 FIXTURE Y POSICIONES</Text>
-          </TouchableOpacity>
+            <TouchableOpacity 
+              style={[globalStyles.input, { backgroundColor: '#f7a684', alignItems: 'center' }]}
+              onPress={() => navigation.navigate('Fixture')}
+            >
+              <Text style={globalStyles.btnText}>🏆 FIXTURE Y POSICIONES</Text>
+            </TouchableOpacity>
 
-          <TouchableOpacity 
-            style={[globalStyles.input, { backgroundColor: '#a69a95', alignItems: 'center' }]} 
-            onPress={() => navigation.navigate('Login')}
-          >
-            <Text style={globalStyles.btnText}>Acceso Organizadores</Text>
-          </TouchableOpacity>
-        </View>
+            <TouchableOpacity 
+              style={[globalStyles.input, { backgroundColor: '#a69a95', alignItems: 'center' }]} 
+              onPress={() => navigation.navigate('Login')}
+            >
+              <Text style={globalStyles.btnText}>Acceso Organizadores</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </ScrollView>
     </View>
-     <View style={styles.footer}>
-        <SponsorCarousel />
-      </View>   
-    
-    </View>
-  );
+
+    <View style={styles.footer}>
+      <SponsorCarousel />
+    </View> 
+  </View>
+);
 }
 
 const styles = StyleSheet.create({
@@ -88,13 +89,13 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 footer: {
-  height: 120, 
+  height: 100,
   width: '100%',
   backgroundColor: '#fff',
   borderTopWidth: 1,
   borderColor: '#eee',
   justifyContent: 'center',
-  paddingBottom: 20, 
+  paddingBottom: Platform.OS === 'ios' ? 25 : 10, 
 },
   map: { height: 200, width: '100%', borderRadius: 10, marginVertical: 10 },
 });
